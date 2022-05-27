8585c1302e640816a2416c640a46daeff5a6a565
Huijun-Cui <3485777351@qq.com>
2022-05-27 14:24:40 +0800

    edit

--- quote_didai2.sol
+++ quote_didai2.sol
@@ -22,7 +22,7 @@ interface IPair {
 }
 
 interface IzumiQuote {
-    function swapAmount(uint128,bytes calldata path) external view returns (uint256 acquire, int24[] memory pointAfterList);
+    function swapAmount(uint128,bytes calldata path) external returns (uint256 acquire, int24[] memory pointAfterList);
 }
 
 // interface Factory {
@@ -49,15 +49,6 @@ contract PriceQuote {
         _;
     }
     
-    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut,uint fee) internal pure returns (uint256 amountOut) {
-        require(amountIn > 0, 'PancakeLibrary: INSUFFICIENT_INPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'PancakeLibrary: INSUFFICIENT_LIQUIDITY');
-        uint amountInWithFee = amountIn.mul(10000 - fee);
-        uint numerator = amountInWithFee.mul(reserveOut);
-        uint denominator = reserveIn.mul(10000).add(amountInWithFee);
-        amountOut = numerator / denominator;
-    }
-    
     function get_reserves(address pair) public view returns(uint reserveA, uint reserveB) {
         (uint reserve0, uint reserve1,) = IPair(pair).getReserves();
         (reserveA, reserveB) = (reserve0, reserve1);
@@ -67,33 +58,5 @@ contract PriceQuote {
         (uint256 ret,) = IzumiQuote(addr).swapAmount(amount, path);
         return ret;
     }
-
-    // function _swap(address izumi_quote,uint128 amount,Params memory param_struct,bytes calldata path) public payable returns (uint256) {
-
-    //     (uint reserve0, uint reserve1) = get_reserves(param_struct.pair);
-    //     uint256 amount3;
-    //     uint256 amount2 = izumi_swap(izumi_quote,amount,path);
-    //     if (param_struct.direction == 0) {
-    //         amount3 = getAmountOut(amount2,reserve0,reserve1,param_struct.fee);
-    //     } else {
-    //         amount3 = getAmountOut(amount2,reserve1,reserve0,param_struct.fee);
-    //     }
-    //     return amount3;
-
-    // }
-
-    // function multi_swap(address izumi_quote,uint128 amount,bytes[] calldata paths,address[] calldata pairs,uint128[] calldata directions,uint128[] calldata fees) public payable returns (uint256[] memory) {
-    //     Params memory param_struct;
-    //     uint256[] memory amount_rets = new uint256[](paths.length);
-    //     for (uint256 i = 0; i < paths.length; i++) {
-    //         param_struct.pair = pairs[i];
-    //         param_struct.direction = directions[i];
-    //         param_struct.fee = fees[i];
-    //         amount_rets[i] = _swap(izumi_quote,amount,param_struct,paths[i]);
-    //     }
-    //     return amount_rets;
-    // }
-
-    
 
 }
