cdea47287fa59e5caec2a6a92c3e61a04af56e8f
Huijun-Cui <3485777351@qq.com>
2022-05-30 11:27:24 +0800

    edit

---
+++ swap.sol
@@ -0,0 +1,179 @@
+pragma solidity >=0.6.0;
+
+
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
+}
+
+
+
+
+
+interface IWETH {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+}
+
+interface IPair {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function token0() external view returns (address);
+}
+
+interface IzumiQuote {
+    function swapAmount(uint128,bytes calldata path) external returns (uint256 acquire, int24[] memory pointAfterList);
+}
+
+interface IAmmSwap {
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);
+}
+
+interface IzumiSwap {
+    
+    function swapAmount(Swap.SwapAmountParams calldata params) external  returns (uint256 cost, uint256 acquire);
+}
+
+
+// interface Factory {
+
+// }
+
+contract Swap {
+    using SafeMath for uint128;
+    using SafeMath for uint256;
+    struct Params {
+        bytes path;
+        address pair;
+        uint128 direction;
+        uint128 fee;
+    }
+
+    struct SwapAmountParams {
+        bytes path;
+        address recipient;
+        uint128 amount;
+        uint256 minAcquired;
+        uint256 deadline;
+    }
+
+    struct SwapAmountParams2 {
+        address amm_router;
+        uint amountIn;
+        uint amountOutMin;
+        address[] path;
+        address to;
+        uint deadline;
+        uint128 direction;
+    }
+
+    address payable public _owner;
+    address public _izumi_addr = 0xBd3bd95529e0784aD973FD14928eEDF3678cfad8;
+    address _WETH = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    constructor() public {
+        _owner = payable(msg.sender);
+    }
+    modifier onlyOwner() {
+        require(_owner == msg.sender, 'Ownable: caller is not the owner');
+        _;
+    }
+
+    function withdraw(uint amount) public onlyOwner {
+        // get the amount of Ether stored in this contract
+        uint amount = address(this).balance;
+        // send all Ether to owner
+        // Owner can receive Ether since the address of owner is payable
+        (bool success, ) = _owner.call{value: amount}("");
+        require(success, "Failed to send Ether");
+    }
+    
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut,uint fee) internal pure returns (uint256 amountOut) {
+        require(amountIn > 0, 'PancakeLibrary: INSUFFICIENT_INPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'PancakeLibrary: INSUFFICIENT_LIQUIDITY');
+        uint amountInWithFee = amountIn.mul(10000 - fee);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(10000).add(amountInWithFee);
+        amountOut = numerator / denominator;
+    }
+    
+    function get_reserves(address pair) public view returns(uint reserveA, uint reserveB) {
+        (uint reserve0, uint reserve1,) = IPair(pair).getReserves();
+        (reserveA, reserveB) = (reserve0, reserve1);
+    }
+
+    function izumi_swap(address addr,uint128 amount,bytes calldata path)  public returns(uint256) {
+        (uint256 ret,) = IzumiQuote(addr).swapAmount(amount, path);
+        return ret;
+    }
+
+    function multi_swap(address izumi_quote,uint128[] calldata amounts,bytes[] calldata paths,address[] calldata pairs,uint128[] calldata direct_fee) public returns (uint256[] memory amount_rets,uint256[] memory amount_inner) {
+        uint256[] memory amount_rets = new uint256[](paths.length);
+        uint256[] memory amount_inner = new uint256[](paths.length);
+        for (uint256 i = 0; i < paths.length; i++) {
+            amount_inner[i] = izumi_swap(izumi_quote,amounts[i],paths[i]);
+            if (i == 0) {
+                amount_rets[i] = izumi_swap(izumi_quote,amounts[i],paths[i]);
+            } else {
+                (uint reserve0, uint reserve1) = get_reserves(pairs[i]);
+                if (direct_fee[i] / 1000 == 0) {
+                    amount_rets[i] = getAmountOut(amount_inner[i],reserve0,reserve1,direct_fee[i] % 1000);
+                } else {
+                    amount_rets[i] = getAmountOut(amount_inner[i],reserve1,reserve0,direct_fee[i] % 1000);
+                }
+            }
+            
+        }
+        return (amount_rets,amount_inner);
+    }
+
+    function profit_swap_u2bnb(SwapAmountParams calldata params,SwapAmountParams2 calldata params2) public {
+        // izumi:busd->wbnb amm:wbnb->busd
+        if (params2.direction == 0) {
+            (uint256 ret,uint256 acquire) = IzumiSwap(_izumi_addr).swapAmount(params);
+            require(params2.to == _owner);
+            IAmmSwap(params2.amm_router).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+                acquire,
+                params2.amountOutMin,
+                params2.path,
+                params2.to,
+                params2.deadline);
+        } else {
+            // amm:busd->bnb izumi:bnb->busd
+            uint[] memory amounts_ret = IAmmSwap(params2.amm_router).swapExactTokensForETH(
+                params2.amountIn,
+                params2.amountOutMin,
+                params2.path,
+                params2.to,
+                params2.deadline);
+            require(params.recipient == _owner);
+            params.amount = amounts_ret[0];
+            (uint256 ret2,uint256 acquire2) = IzumiSwap(_izumi_addr).swapAmount(params);
+        }
+    }
+
+
+    
+
+}
